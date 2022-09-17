ab672f6c6a818e3be547f1c4061a12eed63bfdd4
JennMar <marula9205@hotmail.com>
2022-09-17 13:02:59 -0500

    Se crea archivo ppal

---
+++ saludar.py
@@ -0,0 +1,17 @@
+i = 100
+
+print("**MENÚ**")
+print("0.SALUDAR")
+print("1.DE3SPEDIR")
+print("2.SALIR")
+
+while(i != 0):
+    i = int(input(""))
+    if(i == 0):
+        break
+    elif(i == 1):
+        print("")
+    elif(i == 2):
+        print("")
+    else:
+        print("")
